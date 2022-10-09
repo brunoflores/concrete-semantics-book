dfae45970132858592a4b0d9d02699fd29d35438
Bruno Flores <bruno@brunoflores.com.br>
2022-10-09 11:03:10 +1000

    Work on evens.

---
+++ Even.thy
@@ -0,0 +1,28 @@
+theory Even imports Main begin
+
+inductive ev :: "nat \<Rightarrow> bool" where
+  ev0: "ev 0"
+| evSS: "ev n \<Longrightarrow> ev (Suc (Suc n))"
+
+fun evn :: "nat \<Rightarrow> bool" where
+  "evn 0 = True"
+| "evn (Suc 0) = False"
+| "evn (Suc (Suc n)) = evn n"
+
+lemma "ev (Suc (Suc (Suc (Suc 0))))"
+apply (rule evSS)
+apply (rule evSS)
+apply (rule ev0)
+done
+
+lemma "ev m \<Longrightarrow> evn m"
+apply (induction rule: ev.induct)
+by auto
+
+lemma "evn n \<Longrightarrow> ev n"
+apply (induction rule: evn.induct)
+by (simp_all add: ev0 evSS)
+
+declare ev.intros [simp, intro]
+
+end
