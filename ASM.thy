15ec0a2bb1e5123067bd416aa0195dbb99706b55
Bruno Flores <bruno@brunoflores.com.br>
2023-10-29 13:57:45 +1000

    Answer exercise 3.10.

--- ASM.thy
+++ ASM.thy
@@ -21,23 +21,27 @@ type_synonym stack = "val list"
 
 (* An instruction is executed in the context of a state
    and transforms a stack into a new stack. *)
-fun exec1 :: "instr \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack" where
-  "exec1 (LOADI n) _ stack = n # stack"
-| "exec1 (LOAD x) s stack = (s x) # stack"
-| "exec1 ADD _ (j # i # stack) = (i + j) # stack"
-| "exec1 TIMES _ (j # i # stack) = (i * j) # stack"
+fun exec1 :: "instr \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack option" where
+  "exec1 (LOADI n) _ stack = Some (n # stack)"
+| "exec1 (LOAD x) s stack = Some ((s x) # stack)"
+| "exec1 ADD _ (j # i # stack) = Some ((i + j) # stack)"
+| "exec1 TIMES _ (j # i # stack) = Some((i * j) # stack)"
+(* Stack underflow *)
+| "exec1 ADD _ [] = None"
+| "exec1 ADD _ [_] = None"
+| "exec1 TIMES _ [] = None"
+| "exec1 TIMES _ [_] = None"
 
 (* A list of instructions is executed one by one. *)
-fun exec :: "instr list \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack" where
-  "exec [] _ stack = stack"
-| "exec (i # rest) s stack = exec rest s (exec1 i s stack)"
+fun exec :: "instr list \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack option" where
+  "exec [] _ stack = Some stack"
+| "exec (i # rest) s stack = (case (exec1 i s stack) of
+                                None \<Rightarrow> None \<comment> \<open>Stack underflow\<close>
+                              | Some stack' \<Rightarrow> exec rest s stack')"
 
-lemma "exec [LOADI 5, LOAD ''y'', ADD]
-      <''x'' := 42, ''y'' := 43> [50] = [48, 50]" by simp
-
-lemma exec_append [simp]: "exec (is1 @ is2) s stack
-                           = exec is2 s (exec is1 s stack)"
-by (induct is1 arbitrary: stack, auto)
+lemma "exec [LOADI 5, LOAD ''y'', ADD] <''x'' := 42, ''y'' := 43> [50]
+       = Some [48, 50]"
+by simp
 
 subsection "Compilation"
 
@@ -52,9 +56,14 @@ lemma "comp (Plus (Plus (V ''x'') (N 42)) (V ''z''))
        = [LOAD ''x'', LOADI 42, ADD, LOAD ''z'', ADD]"
 by simp
 
+lemma [simp]: "exec instr1 state stack = Some stack'
+               \<Longrightarrow> exec (instr1 @ instr2) state stack
+               = exec instr2 state stack'"
+by (induction instr1 arbitrary: stack, auto split: option.split)
+
 (* The correctness statement says that executing a compiled expression
-   is the same as putting the value of the expression on the stack: *)
-theorem exec_comp: "exec (comp a) s stack = aval a s # stack"
+   is the same as putting the value of the expression on the stack. *)
+theorem exec_comp: "exec (comp a) state stack = Some (aval a state # stack)"
 by (induction a arbitrary: stack, auto)
 
 end
