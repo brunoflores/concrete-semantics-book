0ea72527f3c20b10a3ff5b02e15cd411a6da6c22
Bruno Flores <bruno@brunoflores.com.br>
2023-10-30 13:21:57 +1000

    Answer exercise 4.2.

--- Star.thy
+++ Star.thy
@@ -4,12 +4,20 @@ inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<R
   refl: "star r x x"
 | step: "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
 
-(* By definition, star r is reflexive and transitive, but we need rule induction to prove: *)
-
+(* By definition, star r is reflexive and transitive,
+   but we need rule induction to prove: *)
 lemma star_trans: "star r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
-apply (induction rule: star.induct)
-apply (assumption)
-apply (metis step)
+  apply (induction rule: star.induct)
+  apply (assumption)
+  apply (metis step)
 done
+
+(* Exercise 4.2 *)
+inductive palindrome :: "'a list \<Rightarrow> bool" where
+  "palindrome []"
+| "palindrome xs \<Longrightarrow> palindrome (a # xs @ [a])"
+
+theorem "palindrome xs \<Longrightarrow> (rev xs) = xs"
+by (induction xs rule: palindrome.induct, auto)
 
 end
