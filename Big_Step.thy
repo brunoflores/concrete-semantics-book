06303194e1844f43cff8b1b1de9c47a8af3bd8b9
Bruno Flores <bruno@brunoflores.com.br>
2023-10-31 09:24:58 +1000

    Continue big-step.

--- Big_Step.thy
+++ Big_Step.thy
@@ -15,16 +15,16 @@ inductive big_step :: "com \<times> state \<Rightarrow> state \<Rightarrow> bool
 | Seq: "        \<lbrakk> (c1, s1) \<Rightarrow> s2; (c2, s2) \<Rightarrow> s3 \<rbrakk> \<Longrightarrow>
                 (c1;; c2, s1) \<Rightarrow> s3"
 
-| IfTrue: "     \<lbrakk> bval b s = True; (c1, s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
+| IfTrue: "     \<lbrakk> bval b s; (c1, s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
                 (IF b THEN c1 ELSE c2, s) \<Rightarrow> t"
 
-| IfFalse: "    \<lbrakk> bval b s = False; (c2, s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
+| IfFalse: "    \<lbrakk> \<not> bval b s; (c2, s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
                 (IF b THEN c1 ELSE c2, s) \<Rightarrow> t"
 
-| WhileFalse: " bval b s = False \<Longrightarrow>
+| WhileFalse: " \<not> bval b s \<Longrightarrow>
                 (WHILE b DO c, s) \<Rightarrow> s"
 
-| WhileTrue: "  \<lbrakk> bval b s1 = True; (c, s1) \<Rightarrow> s2; (WHILE b DO c, s2) \<Rightarrow> s3 \<rbrakk> \<Longrightarrow>
+| WhileTrue: "  \<lbrakk> bval b s1; (c, s1) \<Rightarrow> s2; (WHILE b DO c, s2) \<Rightarrow> s3 \<rbrakk> \<Longrightarrow>
                 (WHILE b DO c, s1) \<Rightarrow> s3"
 
 schematic_goal ex: "(''x'' ::= N 5;; ''y'' ::= V ''x'', s) \<Rightarrow> ?t"
@@ -111,5 +111,121 @@ next
   assume "(c1;; (c2;; c3), s) \<Rightarrow> s'"
   thus "(c1;; c2;; c3, s) \<Rightarrow> s'" by auto
 qed
+
+(* Equivalence of commands *)
+
+(* Informally: two commands [c] and [c'] are equivalent with respect
+   to the big-step semantics when c started in s terminates in t iff
+   c' started in s and also terminates in t. *)
+abbreviation equiv_c :: "com \<Rightarrow> com \<Rightarrow> bool" (infix "\<sim>" 50) where
+  "c \<sim> c' \<equiv> (\<forall>s t. (c, s) \<Rightarrow> t = (c', s) \<Rightarrow> t)"
+
+(* Loop unfolding is an equivalence transformation on programs *)
+lemma unfold_while:
+  "(WHILE b DO c) \<sim> (IF b THEN (c;; WHILE b DO c) ELSE SKIP)" (is "?w \<sim> ?iw")
+proof -
+  \<comment> \<open>to show the equivalence, we look at the derivation tree for\<close>
+  \<comment> \<open>each side and from that construct a derivation tree for the other side\<close>
+  have "(?iw, s) \<Rightarrow> t" if assm: "(?w, s) \<Rightarrow> t" for s t
+  proof -
+    from assm show ?thesis
+    proof cases \<comment> \<open>rule inversion on \<open>(?w, s) \<Rightarrow> t\<close>\<close>
+      case WhileFalse
+      thus ?thesis by blast
+    next
+      case WhileTrue
+      from \<open>bval b s\<close> \<open>(?w, s) \<Rightarrow> t\<close> obtain s' where
+        "(c, s) \<Rightarrow> s'" and "(?w, s') \<Rightarrow> t" by auto
+      \<comment> \<open>now we can build a derivation tree for the \<^text>\<open>IF\<close>\<close>
+      \<comment> \<open>first, the body of the True-branch:\<close>
+      hence "(c;; ?w, s) \<Rightarrow> t" by (rule Seq)
+      \<comment> \<open>then the whole \<^text>\<open>IF\<close>\<close>
+      with \<open>bval b s\<close> show ?thesis by (rule IfTrue)
+    qed
+  qed
+  moreover
+  \<comment> \<open>now the other direction:\<close>
+  have "(?w, s) \<Rightarrow> t" if assm: "(?iw, s) \<Rightarrow> t" for s t
+  proof -
+    from assm show ?thesis
+    proof cases \<comment> \<open>rule inversion on \<open>(?iw, s) \<Rightarrow> t\<close>\<close>
+      case IfFalse
+      hence "s = t" using \<open>(?iw, s) \<Rightarrow> t\<close> by blast
+      thus ?thesis using \<open>\<not>bval b s\<close> by blast
+    next
+      case IfTrue
+      \<comment> \<open>and for this, only the Seq-rule is applicable:\<close>
+      from \<open>(c;; ?w, s) \<Rightarrow> t\<close> obtain s' where
+        "(c, s) \<Rightarrow> s'" and "(?w, s') \<Rightarrow> t" by auto
+      \<comment> \<open>with this information, we can build a derivation tree for \<^text>\<open>WHILE\<close>\<close>
+      with \<open>bval b s\<close> show ?thesis by (rule WhileTrue)
+    qed
+  qed
+  ultimately
+  show ?thesis by blast
+qed
+
+(* Prove automatically *)
+lemma while_unfold:
+  "(WHILE b DO c) \<sim> (IF b THEN c;; WHILE b DO c ELSE SKIP)"
+by blast
+
+lemma triv_if:
+  "(IF b THEN c ELSE c) \<sim> c"
+by blast
+
+lemma commute_if:
+  "(IF b1 THEN (IF b2 THEN c11 ELSE c12) ELSE c2)
+   \<sim>
+   (IF b2 THEN (IF b1 THEN c11 ELSE c2) ELSE (IF b1 THEN c12 ELSE c2))"
+by blast
+
+lemma sim_while_cong_aux:
+  "(WHILE b DO c, s) \<Rightarrow> t \<Longrightarrow> c \<sim> c' \<Longrightarrow> (WHILE b DO c', s) \<Rightarrow> t"
+apply(induction "WHILE b DO c" s t arbitrary: b c rule: big_step_induct)
+  apply blast
+apply blast
+done
+
+(* Establishes that \<sim> is a congruence relation w.r.t. WHILE *)
+lemma sim_while_cong: "c \<sim> c' \<Longrightarrow> WHILE b DO c \<sim> WHILE b DO c'"
+by (metis sim_while_cong_aux)
+
+text \<open>Command equivalence is an equivalence relation, i.e. it is
+reflexive, symmetric, and transitive. Because we used an abbreviation
+above, Isabelle derives this automatically.\<close>
+
+lemma sim_refl:  "c \<sim> c" by simp
+lemma sim_sym:   "(c \<sim> c') = (c' \<sim> c)" by auto
+lemma sim_trans: "c \<sim> c' \<Longrightarrow> c' \<sim> c'' \<Longrightarrow> c \<sim> c''" by auto
+
+(* Determinism *)
+(* A language is deterministic if any two executions of the
+   same command from the same initial state will arrive in the
+   same final state. *)
+
+theorem big_step_determ: "\<lbrakk> (c,s) \<Rightarrow> t; (c,s) \<Rightarrow> u \<rbrakk> \<Longrightarrow> u = t"
+  by (induction arbitrary: u rule: big_step.induct) blast+
+
+(* In Isar: *)
+theorem
+  "(c,s) \<Rightarrow> t  \<Longrightarrow>  (c,s) \<Rightarrow> t'  \<Longrightarrow>  t' = t"
+proof (induction arbitrary: t' rule: big_step.induct)
+  \<comment> \<open>the only interesting case, \<^text>\<open>WhileTrue\<close>:\<close>
+  fix b c s s\<^sub>1 t t'
+  \<comment> \<open>The assumptions of the rule:\<close>
+  assume "bval b s" and "(c,s) \<Rightarrow> s\<^sub>1" and "(WHILE b DO c,s\<^sub>1) \<Rightarrow> t"
+  \<comment> \<open>Ind.Hyp; note the \<^text>\<open>\<And>\<close> because of arbitrary:\<close>
+  assume IHc: "\<And>t'. (c,s) \<Rightarrow> t' \<Longrightarrow> t' = s\<^sub>1"
+  assume IHw: "\<And>t'. (WHILE b DO c,s\<^sub>1) \<Rightarrow> t' \<Longrightarrow> t' = t"
+  \<comment> \<open>Premise of implication:\<close>
+  assume "(WHILE b DO c,s) \<Rightarrow> t'"
+  with \<open>bval b s\<close> obtain s\<^sub>1' where
+      c: "(c,s) \<Rightarrow> s\<^sub>1'" and
+      w: "(WHILE b DO c,s\<^sub>1') \<Rightarrow> t'"
+    by auto
+  from c IHc have "s\<^sub>1' = s\<^sub>1" by blast
+  with w IHw show "t' = t" by blast
+qed blast+ \<comment> \<open>prove the rest automatically\<close>
 
 end
