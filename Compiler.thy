54a54c79d5ee601a9c1d51bd9d8cc4e591c07db2
Bruno Flores <bruno@brunoflores.com.br>
2023-11-01 13:45:39 +1000

    Continue compiler proof.

--- Compiler.thy
+++ Compiler.thy
@@ -212,4 +212,48 @@ value "ccomp
 
 value "ccomp (WHILE Less (V ''u'') (N 1) DO (''u'' ::= Plus (V ''u'') (N 1)))"
 
+(* Preservation of semantics *)
+
+(* For IMP,
+     The machine program should have precisely the same behaviour as the
+     source program.
+
+   1) It should cause the same state change and nothing more than that;
+   2) It should terminate if and only if the source program terminates. *)
+
+(* Show that the compiled code simulates the source code. *)
+lemma ccomp_bigstep:
+  "(c, s) \<Rightarrow> t \<Longrightarrow> ccomp c \<turnstile> (0, s, stk) \<rightarrow>* (size(ccomp c), t, stk)"
+(* Proof is by rule induction on the big-step semantics. *)
+proof(induction arbitrary: stk rule: big_step_induct)
+  case (Assign x a s)
+  show ?case by (fastforce simp: fun_upd_def cong: if_cong)
+next
+  case (Seq c1 s1 s2 c2 s3)
+  let ?cc1 = "ccomp c1"
+  let ?cc2 = "ccomp c2"
+  have "?cc1 @ ?cc2 \<turnstile> (0, s1, stk) \<rightarrow>* (size ?cc1, s2, stk)"
+    using Seq.IH(1) by fastforce
+  moreover
+  have "?cc1 @ ?cc2 \<turnstile> (size ?cc1, s2, stk) \<rightarrow>* (size(?cc1 @ ?cc2), s3, stk)"
+    using Seq.IH(2) by fastforce
+  ultimately show ?case by simp (blast intro: star_trans)
+next
+  case (WhileTrue b s1 c s2 s3)
+  let ?cc = "ccomp c"
+  let ?cb = "bcomp b False (size ?cc + 1)"
+  let ?cw = "ccomp(WHILE b DO c)"
+  have "?cw \<turnstile> (0, s1, stk) \<rightarrow>* (size ?cb, s1, stk)"
+    using \<open>bval b s1\<close> by fastforce
+  moreover
+  have "?cw \<turnstile> (size ?cb, s1, stk) \<rightarrow>* (size ?cb + size ?cc, s2, stk)"
+    using WhileTrue.IH(1) by fastforce
+  moreover
+  have "?cw \<turnstile> (size ?cb + size ?cc, s2, stk) \<rightarrow>* (0, s2, stk)"
+    by fastforce
+  moreover
+  have "?cw \<turnstile> (0, s2, stk) \<rightarrow>* (size ?cw, s3, stk)" by(rule WhileTrue.IH(2))
+  ultimately show ?case by(blast intro: star_trans)
+qed fastforce+
+
 end
