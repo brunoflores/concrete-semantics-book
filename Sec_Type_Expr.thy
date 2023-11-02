196dfedafe3122213382a165fbd4346828918c06
Bruno Flores <bruno@brunoflores.com.br>
2023-11-03 09:48:54 +1000

    Continue sec type system.

--- Sec_Type_Expr.thy
+++ Sec_Type_Expr.thy
@@ -5,6 +5,9 @@ type_synonym level = nat
 class sec =
 fixes sec :: "'a \<Rightarrow> nat"
 
+(* For the sake of showing examples, we arbitrarily choose a specific
+   function for the mapping from variables to security levels:
+   a variable of length n has security level n. *)
 instantiation list :: (type)sec begin
   definition "sec (x :: 'a list) = length x"
 
@@ -30,18 +33,28 @@ instantiation bexp :: sec begin
   instance ..
 end
 
+(* Two syntactic abbreviations to talk about states agreeing on
+   the value of all variables below a certain security level. *)
 abbreviation eq_le :: "state \<Rightarrow> state \<Rightarrow> level \<Rightarrow> bool"
-("(_ = _ '(\<le> _'))" [51,51,0] 50) where
-  "s = s' (\<le> l) == (\<forall> x. sec x \<le> l \<longrightarrow> s x = s' x)"
+("(_ = _ '(\<le> _'))" [51, 51, 0] 50) where
+  "s = s' (\<le> l) \<equiv> (\<forall> x. sec x \<le> l \<longrightarrow> s x = s' x)"
 
 abbreviation eq_less :: "state \<Rightarrow> state \<Rightarrow> level \<Rightarrow> bool"
-("(_ = _ '(< _'))" [51,51,0] 50) where
-  "s = s' (< l) == (\<forall> x. sec x < l \<longrightarrow> s x = s' x)"
+("(_ = _ '(< _'))" [51, 51, 0] 50) where
+  "s = s' (< l) \<equiv> (\<forall> x. sec x < l \<longrightarrow> s x = s' x)"
 
+(* The two abbreviations above make our first two security properties
+   simple and automatic.
+
+   The evaluation of an expression e only depends on variables with
+   level below or equal to sec e. *)
+
+(* Noninterference for arithmetic expressions *)
 lemma aval_eq_if_eq_le:
   "\<lbrakk> s\<^sub>1 = s\<^sub>2 (\<le> l);  sec a \<le> l \<rbrakk> \<Longrightarrow> aval a s\<^sub>1 = aval a s\<^sub>2"
 by (induct a) auto
 
+(* Noninterference for boolean expressions *)
 lemma bval_eq_if_eq_le:
   "\<lbrakk> s\<^sub>1 = s\<^sub>2 (\<le> l);  sec b \<le> l \<rbrakk> \<Longrightarrow> bval b s\<^sub>1 = bval b s\<^sub>2"
 by (induct b) (auto simp add: aval_eq_if_eq_le)
